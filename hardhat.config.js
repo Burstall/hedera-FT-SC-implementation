2330b71cdf07692cbf20a8445c16f85827fe2678
Stowerling <richard@alphacenturi.com>
2022-08-13 14:57:34 +0100

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+	solidity: "0.8.9",
+	gasReporter: {
+	  enabled: true,
+	  currency: "USD",
+	},
+};
